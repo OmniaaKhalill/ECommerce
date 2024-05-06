aeff69f84b19fb95aa1c3b81ec5b420e4e6a1673
Salma-Essam-abdullah <salma.essam3212@gmail.com>
2024-05-06 12:43:46 +0300

    landing-page updates

--- Fronend/src/app/HomePage/home/home.component.ts
+++ Fronend/src/app/HomePage/home/home.component.ts
@@ -4,6 +4,10 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { CategoriesComponent } from '../categories/categories.component';
 import { OfferSectionComponent } from '../offer-section/offer-section.component';
 import { LandingComponent } from '../landing/landing.component';
+import { ProductsComponent } from '../products/products.component';
+import { BlogsComponent } from '../blogs/blogs.component';
+import { ContactUsComponent } from '../contact-us/contact-us.component';
+import { FooterComponent } from '../../core/footer/footer.component';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +17,11 @@ import { LandingComponent } from '../landing/landing.component';
     NavbarComponent,
     LandingComponent,
     CategoriesComponent,
-    OfferSectionComponent
+    OfferSectionComponent,
+    ProductsComponent,
+    BlogsComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
