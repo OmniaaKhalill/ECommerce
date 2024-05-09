158a0eb6af25e1698049005a1192a12a52e9460b
Omnia <omniaakhalill@gmail.com>
2024-05-09 04:55:30 +0300

    login page -v1

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './HomePage/home/home.component';
 import { ShopComponent } from './ShopPage/shop/shop.component';
+import { LoginComponent } from './LoginPage/login/login.component';
+import { RegisterFormComponent } from './LoginPage/register-form/register-form.component';
+import { LoginFormComponent } from './LoginPage/login-form/login-form.component';
 
 export const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent ,children:[
+    { path: "", component: RegisterFormComponent},
+    { path: "Register", component: RegisterFormComponent},
+    { path: "Signin", component: LoginFormComponent}
+  ]},
+
   // { path: "", redirectTo: "shop", pathMatch: "full" },
   // { path: "shop", component: ShopComponent },
 ];
