b6a80070a9426a46d45d53e68913b03585cadac5
Mariem <mariemmohamed1421@gmail.com>
2024-05-10 01:07:10 +0300

    Adding Commits

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './HomePage/home/home.component';
+import { ShopComponent } from './ShopPage/shop/shop.component';
+import { CategoriesPageComponent } from './CategoriesPage/categories-page/categories-page.component';
 
 export const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
+  // { path: "", redirectTo: "home", pathMatch: "full" },
+  // { path: "home", component: HomeComponent },
+  // { path: "", redirectTo: "shop", pathMatch: "full" },
+  // { path: "shop", component: ShopComponent },
+  { path: "", redirectTo: "category", pathMatch: "full" },
+  { path: "category", component: CategoriesPageComponent },
 ];
