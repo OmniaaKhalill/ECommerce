ee777f810f29ec2421350d693a7669e4922c14f0
yasmeen hassan <yasmien960@gmail.com>
2024-05-10 13:31:37 +0300

    adding profile-frontend

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,10 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './HomePage/home/home.component';
 import { ShopComponent } from './ShopPage/shop/shop.component';
+import { TabsComponent } from './ProfilePage/tabs/tabs.component';
+import { OrdersComponent } from './ProfilePage/content/orders/orders.component';
+import { ProfileDetailsComponent } from './ProfilePage/content/profile-details/profile-details.component';
+import { SellerPageComponent } from './ProfilePage/content/seller-page/seller-page.component';
 
 export const routes: Routes = [
   // { path: "", redirectTo: "home", pathMatch: "full" },
-  // { path: "home", component: HomeComponent },
-  { path: "", redirectTo: "shop", pathMatch: "full" },
-  { path: "shop", component: ShopComponent },
+  { path: 'home', component: HomeComponent },
+  /*  { path: "", redirectTo: "shop", pathMatch: "full" },
+  { path: "shop", component: ShopComponent }, */
+
+  {
+    path: '',
+    component: TabsComponent,
+    children: [
+      { path: 'orders', component: OrdersComponent },
+      { path: 'profileDetails', component: ProfileDetailsComponent },
+      {path:'sellerprofileDetails',component:SellerPageComponent},
+    ],
+  },
 ];
