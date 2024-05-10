8c2bd51a43e641b7d9bab9f4cf1cc1d2f8af6b76
Omnia <omniaakhalill@gmail.com>
2024-05-10 14:16:10 +0300

    kan fe coflict so8nan

--- Frontend/src/app/LoginPage/login-form/login-form.component.ts
+++ Frontend/src/app/LoginPage/login-form/login-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
--- Frontend/src/app/LoginPage/register-form/register-form.component.ts
+++ Frontend/src/app/LoginPage/register-form/register-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
