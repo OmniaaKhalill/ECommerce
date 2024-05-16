d21467759f82e675e0bedb9432df31586c65f4a5
Salma-Essam-abdullah <salma.essam3212@gmail.com>
2024-05-16 07:53:16 +0300

    front with back account details and single product page

---
+++ Frontend/src/app/SingleProductPage/single-product/single-product.component.ts
@@ -0,0 +1,82 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+
+import { RouterLink, RouterOutlet, ActivatedRoute } from '@angular/router';
+import { SimilarProductsComponent } from '../similar-products/similar-products.component';
+import { ProductService } from '../../Services/product.service';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-single-product',
+  standalone: true,
+  imports: [CommonModule,RouterLink,RouterOutlet,SimilarProductsComponent],
+  templateUrl: './single-product.component.html',
+  styleUrl: './single-product.component.css'
+})
+export class SingleProductComponent {
+
+constructor(public productService:ProductService , public activatedRoute:ActivatedRoute) {
+  
+}
+
+product:Product = new Product(0,"","",0,0,[],0,"","",0,[]);
+ngOnInit():void{
+  if(this.outOfStock()==true){
+    let qtyInput = <HTMLInputElement>document.getElementById('qty');
+    qtyInput.value = "0";
+  }
+
+  this.activatedRoute.params.subscribe(p=>{this.productService.GetById(p['id']).subscribe(d=>this.product = d) })
+}
+
+
+
+  bigImage: string ="../../../assets/img/Home/product1.jpeg"
+
+  preventReload(event: MouseEvent) {
+   
+    event.preventDefault();
+    
+  }
+
+
+  outOfStock(): boolean {
+   
+    return this.product.numOfProductInStock === 0;
+    
+}
+
+
+
+
+  increment() {
+   
+    let qtyInput = <HTMLInputElement>document.getElementById('qty');
+    let currentValue = parseInt(qtyInput.value);
+    let newValue = currentValue + 1;
+    if (newValue <= this.product.numOfProductInStock) {
+      qtyInput.value = newValue.toString();
+    }
+  }
+
+  decrement() {
+    let qtyInput = <HTMLInputElement>document.getElementById('qty');
+    let currentValue = parseInt(qtyInput.value);
+    let newValue = currentValue - 1;
+    if (newValue >= 1) {
+      qtyInput.value = newValue.toString();
+    }
+  }
+  
+
+  activeTab: string = 'description';
+
+  isActive(tab: string): boolean {
+    return this.activeTab === tab;
+  }
+
+  setActive(tab: string): void {
+    this.activeTab = tab;
+  }
+}
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -8,6 +8,10 @@ import { OrdersComponent } from './ProfilePage/content/orders/orders.component';
 import { ProfileDetailsComponent } from './ProfilePage/content/profile-details/profile-details.component';
 import { SellerPageComponent } from './ProfilePage/content/seller-page/seller-page.component';
 import { EditProductComponent } from './SellerPage/edit-product/edit-product.component';
+import { SingleProductComponent } from './SingleProductPage/single-product/single-product.component';
+import { DescriptionComponent } from './SingleProductPage/description/description.component';
+import { ReviewsComponent } from './SingleProductPage/reviews/reviews.component';
+
 
 export const routes: Routes = [
   // { path: "", redirectTo: "home", pathMatch: "full" },
@@ -19,8 +23,8 @@ export const routes: Routes = [
     path: '',
     component: TabsComponent,
     children: [
-      { path: 'orders', component: OrdersComponent },
-      { path: 'profileDetails', component: ProfileDetailsComponent },
+      { path: 'orders/:id', component: OrdersComponent },
+      { path: 'profileDetails/:id', component: ProfileDetailsComponent },
       {path:'sellerprofileDetails',component:SellerPageComponent},
     ],
   },
@@ -29,7 +33,12 @@ export const routes: Routes = [
   {path:"Add",component:AddProductComponent},
   {path:"Edite/:id",component:EditProductComponent},
   { path: 'home', component: HomeComponent },
-
+  
+  { path: 'product/:id', component: SingleProductComponent,children: [
+      { path: 'reviews/:id', component: ReviewsComponent },
+    { path: 'description/:id', component: DescriptionComponent },
+  
 
 ]
+}]
 
